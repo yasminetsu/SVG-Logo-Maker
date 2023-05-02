25c397c1dc6e6dedd657986a56383734e92c0172
yasminetsu <yasminetsu@gmail.com>
2023-05-01 20:21:27 -0700

    added files

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# svg-logo-maker
+# Object-Oriented Programming Challenge: SVG Logo Maker
+
+<br>
+This is an SVG Logo Maker created for the Monash module 10 challenge! 
+<br>
+
+<br>
+This application does not launch in a browser and instead uses a console line interface via Node.js <br>
+<br>
+Dependencies required: <br>
+<br>
+
+[Node.js](https://www.nodejs.org/en) <br>
+[Inquirer](https://www.npmjs.com/package/inquirer) <br>
+<br>
+
+We also used [Jest](https://www.npmjs.com/package/jest) for testing!<br>
+<br>
+
+Finally, because the main functionality of the project is using ES6 modules for imports and exports and jest is only compatible with common JS 
+imports and exports, the project additionally uses [babel-jest](https://www.npmjs.com/package/babel-jest) to handle the import / export conversions. 
+There are a couple of extra config files that have being created for use by babel-jest. These are, 'jest.config.cjs' and '.babelrc'.
+<br>
+
+
+<br>
+<br>
+
+## User Story
+<code>
+<br>
+AS a freelance web developer <br>
+<br>
+I WANT to generate a simple logo for my projects<br>
+<br>
+SO THAT I don't have to pay a graphic designer<br>
+<br>
+</code>
+
+## Acceptance Criteria
+<br>
+<code>
+GIVEN a command-line application that accepts user input<br>
+<br>
+WHEN I am prompted for text <br>
+<br>
+THEN I can enter up to three characters<br>
+<br>
+WHEN I am prompted for the text color<br>
+<br>
+THEN I can enter a color keyword (OR a hexadecimal number)<br>
+<br>
+WHEN I am prompted for a shape<br>
+<br>
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square<br>
+<br>
+WHEN I am prompted for the shape's color<br>
+<br>
+THEN I can enter a color keyword (OR a hexadecimal number)<br>
+<br>
+WHEN I have entered input for all the prompts<br>
+<br>
+THEN an SVG file is created named `logo.svg`<br>
+<br>
+AND the output text "Generated logo.svg" is printed in the command line<br>
+<br>
+WHEN I open the `logo.svg` file in a browser<br>
+<br>
+THEN I am shown a 300x200 pixel image that matches the criteria I entered<br>
+</code>
+<br>
+<br>
+
+## Tests Passing
+
+## Instructional Video Link
+<br>
+<br>
+
+
---
+++ index.js
@@ -0,0 +1,107 @@
+import { coloursArray } from '../../SVG-Logo-Maker/coloursArray.js/index.js';
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
+import inquirer from 'inquirer';
+import { Triangle, Circle, Square } from './lib/shapes.js';
+
+const canvasWidth = 300;
+const canvasHeight = 200;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: (input) => input.length <= 3,
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter the text color (hexadecimal, i.e.#CD5C5C) or keyword (refer to coloursArray.js)):',
+      validate: (input) => {
+        // check if the input is a valid CSS color name
+        const isColorName = coloursArray.includes(input.toLowerCase());
+        // check if the input is a valid hex color code
+        const isHexCode = /^#[0-9A-F]{6}$/i.test(input);
+        return isColorName || isHexCode;
+      },
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['Circle', 'Triangle', 'Square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter the shape color (hexadecimal or keyword):',
+      validate: (input) => {
+        // check if the input is a valid CSS color name
+        const isColorName = coloursArray.includes(input.toLowerCase());
+        // check if the input is a valid hex color code
+        const isHexCode = /^#[0-9A-F]{6}$/i.test(input);
+        return isColorName || isHexCode;
+      },
+    },
+  ])
+
+  .then((answers) => {
+    let shape;
+    const text = {
+      _attributes: {
+        x: canvasWidth / 2,
+        y: canvasHeight / 1.35, // adjust height of the text, higher # = raise text, this is only for triangle! see 'switch'
+        'text-anchor': 'middle',
+        fill: answers.textColor,
+      },
+      // convert text input to uppercase
+      _text: answers.text.toUpperCase(), 
+    
+      render: function() { 
+        return ` 
+          <text x="${this._attributes.x}" y="${this._attributes.y}" 
+                text-anchor="${this._attributes['text-anchor']}"
+                fill="${this._attributes.fill}" font-size="${fontSize}">
+            ${this._text}
+          </text>
+        `;
+      },
+    };
+    // adjust shape dimensions here, this is where the 'new' shape is created dependent on selection
+    let fontSize;
+    switch (answers.shape) {
+      case 'Circle':
+        const circleRadius = Math.min(canvasWidth, canvasHeight) * 0.45;        // increase radius
+        shape = new Circle(canvasWidth / 2, canvasHeight / 2, circleRadius);
+        text._attributes.y = canvasHeight / 1.65;                               // custom text height to suit shape
+        fontSize = 58;                                                          // custom font size to suit shape
+        break;
+        case 'Triangle':                                   
+          const triangleHeight = Math.min(canvasWidth, canvasHeight) * 1.1;     // increase the height
+          shape = new Triangle(canvasWidth / 2, canvasHeight / 2, triangleHeight);
+          fontSize = 52;                                                        // custom font size to suit shape
+          break;
+      case 'Square':
+        const squareSize = Math.min(canvasWidth, canvasHeight) * 0.8;           // percentage of area the box takes of canvas
+        shape = new Square(canvasWidth / 2, canvasHeight / 2, squareSize);
+        text._attributes.y = canvasHeight / 1.65;                               // custom text height to suit shape
+        fontSize = 60;                                                          // custom font size to suit shape
+        break;
+    }
+
+    const svgData = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="${canvasWidth}" height="${canvasHeight}">
+        ${shape.render(answers.shapeColor)}
+        ${text.render()}
+      </svg>`;
+
+    fs.writeFileSync(`${__dirname}/logo.svg`, svgData.toString());
+
+    console.log('Generated logo.svg');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
